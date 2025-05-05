280266ccc023a8d1076eb36699befc03c2edfbf7
Dinuki85 <prarthanadinuki2@gmail.com>
2025-05-05 13:09:45 +0530

    Added details of the Person model

--- src/main/java/com/backend/Backend/model/Person.java
+++ src/main/java/com/backend/Backend/model/Person.java
@@ -1,4 +1,13 @@
 package com.backend.Backend.model;
 
-public class Person {
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+
+@MappedSuperclass
+public abstract class Person {
+    @Id
+    private String id;
+    private String name;
+    private int age;
+    private String gender;
 }
