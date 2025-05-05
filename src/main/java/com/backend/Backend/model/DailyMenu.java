00330953aa85cc731becbcb7bab211c2afffd780
Dinuki85 <prarthanadinuki2@gmail.com>
2025-05-05 13:13:42 +0530

    Added deyails of the Daily menu model

--- src/main/java/com/backend/Backend/model/DailyMenu.java
+++ src/main/java/com/backend/Backend/model/DailyMenu.java
@@ -1,4 +1,23 @@
 package com.backend.Backend.model;
 
+import jakarta.persistence.*;
+
+import java.util.Date;
+import java.util.List;
+
+@Entity
 public class DailyMenu {
+    @Id
+    private String id;
+    private Date date;
+
+    @ManyToOne
+    @JoinColumn(name = "canteen_id")
+    private Canteen canteen;
+
+    @ManyToMany
+    @JoinTable(name = "menu_food_items",
+            joinColumns = @JoinColumn(name = "menu_id"),
+            inverseJoinColumns = @JoinColumn(name = "food_item_id"))
+    private List<FoodItem> foodItems;
 }
