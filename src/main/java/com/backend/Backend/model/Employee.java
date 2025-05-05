fe9267445552cecae40f3c76ce38d910b159689f
Dinuki85 <prarthanadinuki2@gmail.com>
2025-05-05 13:11:18 +0530

    Added details of the Employee model

--- src/main/java/com/backend/Backend/model/Employee.java
+++ src/main/java/com/backend/Backend/model/Employee.java
@@ -1,4 +1,19 @@
 package com.backend.Backend.model;
 
-public class Employee {
+import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+
+import java.util.List;
+
+@Entity
+public class Employee extends Person {
+    private String jobPosition;
+
+    @ManyToMany
+    @JoinTable(name = "employee_canteens",
+            joinColumns = @JoinColumn(name = "employee_id"),
+            inverseJoinColumns = @JoinColumn(name = "canteen_id"))
+    private List<Canteen> canteens;
 }
