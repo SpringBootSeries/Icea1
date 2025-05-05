b9d09b9ccb8d4c7fee99278339746be487f748d8
Dinuki85 <prarthanadinuki2@gmail.com>
2025-05-05 13:12:05 +0530

    Added details of the Canteen model

--- src/main/java/com/backend/Backend/model/Canteen.java
+++ src/main/java/com/backend/Backend/model/Canteen.java
@@ -1,4 +1,23 @@
 package com.backend.Backend.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToMany;
+
+import java.util.List;
+
+@Entity
 public class Canteen {
+    @Id
+    private String id;
+    private String name;
+    private String location;
+
+    @OneToMany(mappedBy = "canteen")
+    private List<DailyMenu> dailyMenus;
+
+    @ManyToMany(mappedBy = "canteens")
+    private List<Employee> employees;
 }
+
