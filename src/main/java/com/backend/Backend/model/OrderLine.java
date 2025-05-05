3b8224dd69b73edad3663d89a9e35502d86c4ee2
Dinuki85 <prarthanadinuki2@gmail.com>
2025-05-05 13:15:25 +0530

    Added details of the OrderLine model

--- src/main/java/com/backend/Backend/model/OrderLine.java
+++ src/main/java/com/backend/Backend/model/OrderLine.java
@@ -1,4 +1,22 @@
 package com.backend.Backend.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+@Entity
 public class OrderLine {
+    @Id
+    private String id;
+    private int quantity;
+
+    @ManyToOne
+    @JoinColumn(name = "order_id")
+    private FoodOrder order;
+
+    @ManyToOne
+    @JoinColumn(name = "food_item_id")
+    private FoodItem foodItem;
 }
+
