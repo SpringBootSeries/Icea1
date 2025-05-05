edf8eaaf262b8e580cfa7dc6a487730f2af9a57f
Dinuki85 <prarthanadinuki2@gmail.com>
2025-05-05 13:14:38 +0530

    added details of the FoodOrder model

--- src/main/java/com/backend/Backend/model/FoodOrder.java
+++ src/main/java/com/backend/Backend/model/FoodOrder.java
@@ -1,4 +1,21 @@
 package com.backend.Backend.model;
 
+import jakarta.persistence.*;
+
+import java.util.Date;
+import java.util.List;
+
+@Entity
 public class FoodOrder {
+    @Id
+    private String id;
+    private Date orderDate;
+
+    @ManyToOne
+    @JoinColumn(name = "student_id")
+    private Student student;
+
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
+    private List<OrderLine> orderLines;
 }
+
