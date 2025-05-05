8b91a8d75953703eac26d84fbcec4f5a8766a3e5
Dinuki85 <prarthanadinuki2@gmail.com>
2025-05-05 13:12:51 +0530

    Ädded details of the Food Item mode

--- src/main/java/com/backend/Backend/model/FoodItem.java
+++ src/main/java/com/backend/Backend/model/FoodItem.java
@@ -1,4 +1,24 @@
 package com.backend.Backend.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToMany;
+
+import java.util.List;
+
+@Entity
 public class FoodItem {
+    @Id
+    private String id;
+    private String name;
+    private double price;
+    private double weight;
+
+    @ManyToMany(mappedBy = "foodItems")
+    private List<DailyMenu> menus;
+
+    @OneToMany(mappedBy = "foodItem")
+    private List<OrderLine> orderLines;
 }
+
